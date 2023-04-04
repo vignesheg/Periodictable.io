b1f1d6ede2b53b9a3d867b235d07205232fcbdd0
vignesheg <108560341+vignesheg@users.noreply.github.com>
2023-04-04 15:05:54 +0530

    uploaded files

---
+++ index.js
@@ -0,0 +1,30 @@
+function getdetails(){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET","http://localhost/periodicelementstable/data.json",false);
+    xmlhttp.send();
+    if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+        var result  = xmlhttp.responseText;
+        var obj = JSON.parse(result);
+    }
+
+    var searchvalues = document.forms["searchform"]["searchvalue"].value;
+   
+    var ordres = obj.find(function(order){
+        return order.Symbol === searchvalues;
+    })
+    var headerdata="";
+    var elementdata="";
+  
+    for(key in ordres){
+         elementdata += '<li class="list-group-item list-group-item-action"><b style="font-size:18px;">'+key+'</b>   =   '+ordres[key]+'</li>';
+    }
+
+    document.getElementById("elementlist").innerHTML = elementdata;
+    console.log(ordres);    
+
+    var x = document.getElementById("searchbt");
+    x.addEventListener('click',function hideheader(){
+        document.getElementById("header").style.display="";
+    })
+}
+
